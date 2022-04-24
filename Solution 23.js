682a929618f18e4562a54b5141dcd00aa879159f
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2022-04-24 10:26:47 +0100

    Solution 23
    //Write a function that will check if two given characters are the same case.
    //If either of the characters is not a letter, return -1
    //If both characters are the same case, return 1
    //If both characters are letters, but not the same case, return 0

---
+++ Solution 23.js
@@ -0,0 +1,5 @@
+//Write a function that will check if two given characters are the same case.
+//If either of the characters is not a letter, return -1
+//If both characters are the same case, return 1
+//If both characters are letters, but not the same case, return 0
+checkCase = (a,b) => /[a-z]/i.test(a) && /[a-z]/i.test(b) ? Number(/[a-z]/.test(a) == /[a-z]/.test(b)) : - 1
