c85746085fe2cc75767580ef8c64ddc0b278234a
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-05-12 18:30:02 +0100

    reversearr

---
+++ ReverseArr.js
@@ -0,0 +1,18 @@
+
+
+//arr -> always an array, never empty
+//returns -> arr reversed
+//arr [2,4,4] -> [4,4,2]
+
+
+function reverseArr(arr){
+
+    let reversedArray = arr.reverse()
+
+    return reversedArray
+}
+
+
+
+console.log(reverseArr([1,2]), '[2,1]')
+console.log(reverseArr([1,3]), '[3,1]')
