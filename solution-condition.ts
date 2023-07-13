08d7425a1cc6aa785d7ae29428351b9e5dcdebb8
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-07-13 16:23:31 +0100

    condition-ts

---
+++ solution-condition.ts
@@ -0,0 +1,5 @@
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+
+export function saleHotdogs(n: number): number {
+  return n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n * 90;
+}
